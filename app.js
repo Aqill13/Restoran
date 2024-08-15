61d319b70600fa95a0159b73b93b6f72037a6c8a
aqil <aqilkalbiyev01@gmail.com>
2024-08-15 22:55:09 +0400

    menu page changes

--- app.js
+++ app.js
@@ -1,39 +1,42 @@
-
 //? scrollY > 0 navbar sticky
 
 $(document).ready(function () {
-    $(window).scroll(function () {
-        var scroll = $(window).scrollTop();
-        var navbar = $(".navbar");
+  $(window).scroll(function () {
+    var scroll = $(window).scrollTop();
+    var navbar = $(".navbar");
 
-        if (scroll > 0) {
-            navbar.addClass("sticky");
-        } else {
-            navbar.removeClass("sticky");
-        }
-    });
+    if (scroll > 0) {
+      navbar.addClass("sticky");
+    } else {
+      navbar.removeClass("sticky");
+    }
+  });
 });
 
 // ? Bolum 7 Qaleriya
-$(document).ready(function() {
-
-    $(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function() {
-      $(this).parent().children(".overlay").show();
-  
-    });
-  
-  
-    $(`[unique-script-id="w-w-dm-id"] .close`).click(function() {
-      $(".overlay").hide();
-    });
+$(document).ready(function () {
+  $(`[unique-script-id="w-w-dm-id"] .btn-box`).click(function () {
+    $(this).parent().children(".overlay").show();
   });
 
+  $(`[unique-script-id="w-w-dm-id"] .close`).click(function () {
+    $(".overlay").hide();
+  });
+});
+
 //? Bolum 8 Rezervasiya
+
 function selectTable() {
-  const selectedTable = document.querySelector('input[name="tableNumber"]:checked');
-  if (selectedTable) {
-      document.getElementById('tableNumberInput').value = selectedTable.value;
+  var selectedTable = $('input[name="tableNumber"]:checked');
+
+  if (selectedTable.length) {
+    $('#tableNumberInput').val(selectedTable.val());
+    $('#tableModal').modal('hide');
   }
-  var modal = bootstrap.Modal.getInstance(document.getElementById('tableModal'));
-  modal.hide();
+  else{
+    $('.warningAlertReservationTable').show();
+    $('#tableNumberInput').on('focus', function () {
+      $('.warningAlertReservationTable').hide();
+    })
+  }
 }
